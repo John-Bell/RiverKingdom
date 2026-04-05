a22d4eeb72cc7422d4a4ca1a67996b9a0d3d0720
John-Bell <john@Edmonds-Bell.co.uk>
2026-04-05 14:13:29 +0100

    fixed (#6)
    * fixed
    
    * removed
    
    ---------
    
    Co-authored-by: John-Bell <John-Bell@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-  // THIS IS THE CRITICAL ADDITION:
-  // Tells Vite we are hosted in a sub-folder on GitHub Pages
   base: '/RiverKingdom/',
-  
   root: 'src',
   publicDir: '../public',
   build: {
     outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/scripts/index.html')
-      }
-    }
+    emptyOutDir: true
   }
 });
