bdcbd044a760ef79da7edac5d53b68a064559d78
John-Bell <John-Bell@users.noreply.github.com>
2026-04-05 12:40:37 +0100

    fix-location
    Fix location

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { fileURLToPath } from 'url';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  // THIS IS THE CRITICAL ADDITION:
+  // Tells Vite we are hosted in a sub-folder on GitHub Pages
+  base: '/RiverKingdom/',
+  
   root: 'src',
   publicDir: '../public',
   build: {
