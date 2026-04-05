9879de7d5a4ac7279c8315fd204ce0b53b39dacf
John-Bell <John-Bell@users.noreply.github.com>
2026-04-05 13:11:58 +0100

    try fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  base: '/RiverKingdom/',
   root: 'src',
   publicDir: '../public',
   build: {
     outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/scripts/index.html')
-      }
-    }
+    emptyOutDir: true
   }
 });
